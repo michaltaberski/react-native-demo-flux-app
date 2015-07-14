37261588c76d02f37ae58f4ba5b90fb954940f02
michaltaberski <m.taberski@gmail.com>
2015-07-14 10:59:43 +0200

    simple demo page added

---
+++ App/Components/demo_page.js
@@ -0,0 +1,65 @@
+var React = require('react-native');
+
+const {
+  View,
+  Text,
+  StyleSheet,
+} = React;
+
+const {
+  pannable,
+} = require('react-native-gesture-recognizers');
+
+var styles = StyleSheet.create({
+  block: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'orange',
+    top: 80,
+    alignSelf: 'center',
+  },
+
+});
+
+class Box {
+  render() {
+    return (
+      <View style={styles.block}>
+        <Text>This is just a block.</Text>
+      </View>
+    );
+  }
+}
+
+class Demo extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      transform: [],
+    };
+  }
+
+  onPan({ absoluteChangeX, absoluteChangeY }) {
+    this.setState({
+      transform: [
+        {translateX: absoluteChangeX},
+        {translateY: absoluteChangeY}
+      ]
+    });
+  }
+
+  render() {
+    return(
+      <Box
+        onPan={this.onPan.bind(this)}
+      />
+    );
+  }
+
+}
+
+module.exports = {
+  component: Demo,
+  title: 'Demo page',
+};
