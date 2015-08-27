cea11d697b9856feb5e4930393ec75ac03249faf
michaltaberski <m.taberski@gmail.com>
2015-08-27 10:30:56 +0200

    dynamic add of animated squares

--- App/Components/demo_page.js
+++ App/Components/demo_page.js
@@ -1,5 +1,8 @@
 var React = require('react-native');
 
+var Button = require('./button');
+var _ = require('lodash');
+
 const {
   View,
   Text,
@@ -8,13 +11,13 @@ const {
   PanResponder,
 } = React;
 
-const SQUARE_DIMENSIONS = 150;
+const SQUARE_DIMENSION = 100;
 const SWIPE_THRESHOLD = 120;
 
 var styles = StyleSheet.create({
   square: {
-    width: SQUARE_DIMENSIONS,
-    height: SQUARE_DIMENSIONS,
+    width: SQUARE_DIMENSION,
+    height: SQUARE_DIMENSION,
     backgroundColor: 'orange',
   },
   container: {
@@ -24,19 +27,29 @@ var styles = StyleSheet.create({
   },
 });
 
-let AnimatedView = Animated.View;
-
-class Square {
+class Square extends React.Component {
 
   constructor() {
-
-    this.state = {}
-    this.state.pan = new Animated.ValueXY();
+    super();
+    // SIMPLE BLOG POST
+    // http://browniefed.com/blog/2015/08/15/react-native-animated-api-with-panresponder/
+    // A BIT MORE ADVANCED CODE REPO
+    // https://github.com/brentvatne/react-native-animated-demo-tinder
+    this.state = {
+      pan: new Animated.ValueXY(),
+      enter: new Animated.Value(0.5),
+    }
 
   }
 
+  componentDidMount() {
+    Animated.spring(
+      this.state.enter,
+      { toValue: 1, friction: 4 }
+    ).start();
+  }
+
   componentWillMount() {
-    // http://browniefed.com/blog/2015/08/15/react-native-animated-api-with-panresponder/
     this._panResponder = PanResponder.create({
       onMoveShouldSetResponderCapture: () => true, //Tell iOS that we are allowing the movement
       onMoveShouldSetPanResponderCapture: () => true, // Same here, tell iOS that we allow dragging
@@ -99,6 +112,9 @@ class Square {
             translateY: this.state.pan.y
           },
           {
+            scale: this.state.enter
+          },
+          {
             rotate: this.state.pan.x.interpolate({
               inputRange: [-200, 0, 200],
               outputRange: ['-30deg', '0deg', '30deg'],
@@ -116,26 +132,51 @@ class Square {
   }
   render() {
     return (
-      <AnimatedView
+      <Animated.View
         style={[this.getStyle(), this.props.style]}
         {...this._panResponder.panHandlers}
       >
-        <Text>This is just a block.</Text>
-      </AnimatedView>
+        <Text>Test</Text>
+      </Animated.View>
     );
   }
 }
 
 
 class Demo extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      squareCount: 1
+    };
+
+  }
+
+  addSquare() {
+    this.setState({
+      squareCount: this.state.squareCount + 1,
+    });
+  }
+
   render() {
     return(
       <View style={styles.container} >
-        <Square />
-        <Square style={{backgroundColor: 'blue'}}/>
+        {
+          _.times(this.state.squareCount, () => {
+            return (
+              <Square style={{backgroundColor: 'blue'}}/>
+            );
+          })
+        }
+        <View>
+          <Button onPress={this.addSquare.bind(this)} label='Add Square' style={{backgroundColor: 'red'}}/>
+        </View>
       </View>
     );
   }
+
 }
 
 module.exports = {
